246162b6d67508224dc0b09920b68c5060406ed0
Aamir Hussain <aamirbhat382@gmail.com>
2024-07-08 10:11:49 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// document.addEventListener('DOMContentLoaded', () => {
+//     const params = new URLSearchParams(window.location.search);
+//     const restaurantName = params.get('restaurant_name') || 'Restaurant Name';
+    
+//     document.getElementById('restaurant-name').textContent = restaurantName;
+//     document.getElementById('restaurant-name-hero').textContent = `Welcome to ${restaurantName}`;
+//   });
+  
